b7959faf707d673404f6d305574ce6c1c0624af0
odomaj <odomaj@clarkson.edu>
2024-10-23 17:21:52 -0400

    updated venv script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 2048
+
+## Setting up virtual environment
+Run make_venv.py from within the scripts directory to make the virtual environment
+Activate the virtual environment in your shell using venv/Scripts/activate
+Run make_venv.py from within the scripts directory again to install all dependencies
--- scripts/make_venv.py
+++ scripts/make_venv.py
@@ -1,14 +1,27 @@
 import subprocess
+import sys
+from os import getcwd
+from pathlib import Path
 
 
-def make_venv():
+def make_venv() -> None:
     subprocess.run(["python", "-m", "venv", "../venv"])
+    print("made virtual environment venv")
 
 
-def install_packages():
-    subprocess.run(["pip", "install", "numpy"])
+def install_packages() -> None:
+    subprocess.run(["pip", "install", "-r", "../requirements.txt"])
+    print("installed packages")
 
 
 if __name__ == "__main__":
-    subprocess.run(["python", "-m", "venv", "../venv"])
-    subprocess.run(["../venv/Scripts/activate.bat"])
+    if Path(__file__).parent.absolute() != Path(getcwd()).absolute():
+        print("must be in scripts directory, exiting")
+        exit(0)
+    if (
+        Path(sys.executable).parent.parent.parent.absolute()
+        == Path(__file__).parent.parent.absolute()
+    ):
+        install_packages()
+    else:
+        make_venv()
