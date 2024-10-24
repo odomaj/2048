440abf39c80d310a79b1a29893d7d953805e6010
odomaj <odomaj@clarkson.edu>
2024-10-24 09:56:32 -0400

    improved make_venv

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # 2048
 
 ## Python Version
-This repo is designed to run on Google Colab, which runs [Python 3.10.12](https://www.python.org/downloads/release/python-31012/)
+This repo is designed to run on [Google Colab](https://colab.google/), which runs [Python 3.10.12](https://www.python.org/downloads/release/python-31012/)
 * [Windows Build Instructions](https://www.youtube.com/watch?v=GqZT9EY4MGQ)
 
 ## Making a Virtual Environment
-1. Run make_venv.py from within the scripts directory to make the virtual environment
-2. Activate the virtual environment in your shell 
+1. Run make_venv.py from within the scripts directory to make the virtual environment and install all dependencies
+2. Activate the virtual environment
     1. in your shell current shell using venv/Scripts/activate
     2. in vscode enter 'ctrl' + 'shift' + 'p', select 'Python: Select Interpreter', enter ./venv/Scripts/python.exe
-3. Run make_venv.py from within the scripts directory again to install all dependencies
--- scripts/make_venv.py
+++ scripts/make_venv.py
@@ -1,17 +1,42 @@
+"""makes a virtual environment venv in the root directory of the repo
+and installs all dependencies in the requirements.txt file at the root
+directory of the repo"""
+
 import subprocess
 import sys
-from os import getcwd
 from pathlib import Path
 
 
 def make_venv() -> None:
-    subprocess.run(["python", "-m", "venv", "../venv"])
-    print("made virtual environment venv")
+    """builds a virtual environment in the root directory of the repo named
+    venv"""
+    subprocess.run(
+        [
+            "python",
+            "-m",
+            "venv",
+            Path(__file__).parent.parent.joinpath("venv").absolute(),
+        ]
+    )
 
 
 def install_packages() -> None:
-    subprocess.run(["pip", "install", "-r", "../requirements.txt"])
-    print("installed packages")
+    """uses pip to install all of the dependencies specified in the
+    requirements.txt of the root directory in the virtual environment venv"""
+    subprocess.run(
+        [
+            Path(__file__)
+            .parent.parent.joinpath("venv/Scripts/python.exe")
+            .absolute(),
+            "-m",
+            "pip",
+            "install",
+            "-r",
+            Path(__file__)
+            .parent.parent.joinpath("requirements.txt")
+            .absolute(),
+        ]
+    )
 
 
 if __name__ == "__main__":
@@ -22,9 +47,7 @@ if __name__ == "__main__":
     ):
         print("[WARNING] python 3.10.12 is recommended")
 
-    if Path(__file__).parent.absolute() != Path(getcwd()).absolute():
-        print("[ERROR] must be in scripts directory, exiting")
-        exit(0)
+    # check if the current running version of python is sourced from venv
     if (
         Path(sys.executable).parent.parent.parent.absolute()
         == Path(__file__).parent.parent.absolute()
@@ -32,3 +55,4 @@ if __name__ == "__main__":
         install_packages()
     else:
         make_venv()
+        install_packages()
